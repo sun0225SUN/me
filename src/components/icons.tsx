52ce51cc47b9e8fa7b2ce977544ee3a743215db4
Guoqi Sun <sun412926@gmail.com>
2024-10-06 16:32:15 +0800

    feat: upload my info

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -23,6 +23,12 @@ export const Icons = {
       />
     </svg>
   ),
+  twitter: (props: IconProps) => (
+    <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" {...props}>
+      <title>Twitter</title>
+      <path d="M22 4s-.7 2.1-2 3.4c1.6 10-9.4 17.3-18 11.6 2.2.1 4.4-.6 6-2C3 15.5.5 9.6 3 5c2.2 2.6 5.6 4.1 9 4-.9-4.2 4-6.6 7-3.8 1.1 0 3-1.2 3-1.2z" />
+    </svg>
+  ),
   youtube: (props: IconProps) => (
     <svg
       width="32px"
